cd7e8658444665e55664cef2618273544df7cbc5
gksk <Daum_c0m@naver.com>
2024-01-30 19:43:39 +0900

    초기 커밋.

---
+++ 02_cout cin endl/02_cout cin endl.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numeber = 0;
+
+    cin >> numeber;
+
+    cout << "입력한 숫자는 " << numeber << " 입니다." << endl;
+
+    return 0;
+}
+
+/* 
+002 cout cin endl
+
+- cout: 출력할 때 사용, << 를 이용함.
+- cin : 입력할 때 사용, >> 를 이용함.
+*/
